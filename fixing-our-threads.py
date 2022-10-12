63e68b3f726263fd54618a8235d80d440f766fd2
RonnyFray <ronnyfray@gmail.com>
2022-10-12 13:16:48 -0700

    Thread-safe print message to console Add citation for references used

--- fixing-our-threads.py
+++ fixing-our-threads.py
@@ -1,5 +1,11 @@
 # Programming Assignment 1: Fixing Our Threads
 
+"""
+Resources used:
+-   Examples in course repository: https://github.com/TeachingData/basic-python
+-   Thread-Safe Printing: https://superfastpython.com/thread-safe-print-in-python/
+"""
+
 import sys
 import threading
 import time
@@ -9,14 +15,14 @@ def thread_function(name, message):
 
     # make sure the file exists before attempting to read
     if os.path.exists('names.txt'):
-        with open("names.txt", 'r+') as namesfile:
+        with open("names.txt", 'r+') as namesfile, lock:
             # print the file before writing a message
             print(namesfile.read())
     else:
         print("The file does not exist.")
 
     # by appending a file, it is locked & cannot be opened by another thread
-    with open('names.txt', 'a') as namesfile, lock:
+    with open('names.txt', 'a') as namesfile:
         namesfile.write(f"Thread name is {name}\t{message}\n")
 
 
@@ -26,11 +32,13 @@ def sleep_function(name):
 
 
 if __name__ == "__main__":
-    # basic synchronization using Lock(); not needed due to append locking file
+    # basic synchronization using Lock()
     lock = threading.Lock()
 
     for n in sys.argv[1:]:
+        lock.acquire()  # used to thread-safe print
         message = input("Please enter your message: ")
+        lock.release()
         prth = threading.Thread(target=thread_function, args=(n,message))
         prth.start()
 
